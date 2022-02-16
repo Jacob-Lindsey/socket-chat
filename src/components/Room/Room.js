c3129edca1081f8546a20d4063188fb5d1303cac
Jake <jacob.lindsey88@gmail.com>
2022-02-16 13:47:34 -0600

    Made chat page responsive. Added sender id to chat messages.

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { AiOutlineSend } from "react-icons/ai";
 import useChat from "../../hooks/useChat";
 import styles from "./Room.module.css";
 
@@ -18,36 +19,55 @@ const Room = (props) => {
         setNewMessage('');
     };
 
+    const handleEnterKeySubmit = (e) => {
+        if (e.key === 'Enter' && newMessage !== '') {
+            e.preventDefault();
+            e.stopPropagation();
+            handleSendMessage();
+        }
+    };
+
     return (
-        <div className={styles.roomContainer}>
-            <h1 className={styles.roomName}>Room: {roomId}</h1>
-            <div className={styles.messagesContainer}>
+        <main className={styles.roomContainer}>
+            <h1 className={styles.roomName}># {roomId}</h1>
+            <section className={styles.messagesContainer}>
                 <ol className={styles.messagesList}>
                     {messages.map((message, index) => (
-                        <li
+                        <li 
                             key={index}
-                            className={`${styles.messageItem} ${message.ownedByCurrentUser ? styles.myMessage : styles.recievedMessage}`}
-                        >
-                            {message.body}
+                            className={`${styles.messageItem} ${message.ownedByCurrentUser ? styles.myMessage : styles.recievedMessage}`}>
+                            <p className={styles.messageText}>
+                                {message.body}
+                            </p>
+                            <small className={styles.messageSender}>{message.senderId}</small>
                         </li>
                     ))}
                 </ol>
-            </div>
-            <div className={styles.messageInputContainer}>
-                <input
-                    type="text"
-                    value={newMessage}
-                    onChange={handleNewMessageChange}
-                    placeholder="Write message..."
-                    className={styles.messageInputField}
-                />
-                <button
-                    onClick={handleSendMessage} className={styles.sendMessageButton}
-                >
-                    Send
-                </button>
-            </div>
-        </div>
+            </section>
+            <footer className={styles.footer}>
+                <div className={styles.messageInputContainer}>
+                    <input
+                        type="text"
+                        value={newMessage}
+                        onChange={handleNewMessageChange}
+                        onKeyDown={(e) => handleEnterKeySubmit(e)}
+                        placeholder="Write message..."
+                        className={styles.messageInputField}
+                    />
+                    {newMessage !== '' ? 
+                        <button
+                        onClick={handleSendMessage} 
+                        className={styles.sendMessageButton}
+                        >
+                            <AiOutlineSend />
+                        </button>
+                            :
+                        null
+                    }
+                    
+                </div>
+            </footer>
+        </main>
     )
 
 };
