457b62b12af28667daae07bfd9d979cdc36c5277
Jake <jacob.lindsey88@gmail.com>
2022-02-25 12:27:42 -0600

    Disabling database calls due to bugs.

--- src/models/messageModel.js
+++ src/models/messageModel.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
     body: { type: String, required: true },
-    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    room: { type: Schema.Types.ObjectId, ref: "Room", required: true },
+    user: { type: String, required: true },
+    room: { type: String, required: true },
     timestamp: { type: String, required: true }
 });
 
